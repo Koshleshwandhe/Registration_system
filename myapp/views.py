91bab1b256f7a8ffe4386506d71d2674ec634470
koshlesh <kunalwandhe00@gmail.com>
2024-05-03 12:06:31 +0530

    integrateing api and add dropdown

--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render,redirect
+import requests
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from . models import User
+from .models import User
+from .authentication import generate_auth_token
 
-# Create your views here.
 def login(request):
     if request.method == 'POST':
         id = request.POST.get('id')
         password = request.POST.get('password')
-        user = User.objects.filter(id=id, password=password).first()
-        if user:
-            return render(request,'main_screen.html')
+        user_type = request.POST.get('user_type')
+
+        # Generate the base64 authentication token
+        auth_token = generate_auth_token(id, password)
+
+        # Make a GET request to the API endpoint with the obtained token
+        url = f"https://centrum-backend.vercel.app/login/{user_type}/dashboard/ALL"
+        headers = {'Authorization': f'Basic {auth_token}'}
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            # Authentication successful, redirect to main screen
+            data = response.json()
+            return render(request, 'main_screen.html', {'data': data})
         else:
-            return HttpResponse("Invalid username or password.'")
-    return render(request,"login.html")
+            # Authentication failed, return error message
+            return HttpResponse("Invalid username or password.")
+    return render(request, "login.html")
 
 def signin(request):
     if request.method == 'POST':
@@ -26,8 +39,6 @@ def signin(request):
         return redirect('login')
     return render(request,"signin.html")
 
-
 def main_screen(request):
-    # Fetch all user details from the database
-    users = User.objects.all()
-    return render(request, 'main_screen.html', {'users': users})
+    # Render the main screen template without data initially
+    return render(request, 'main_screen.html')
